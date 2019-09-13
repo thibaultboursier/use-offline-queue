fb78b06aebe2b3a542484f8a0b22d43f090228d9
thibaultboursier <boursier.thibault@gmail.com>
2019-09-13 19:57:43 +0200

    chore(npm): add some dependencies

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/enzyme.config'],
+};
